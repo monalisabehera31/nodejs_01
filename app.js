c46bf3110d23605e092219ad8251957c9beddf45
monalisa behera <monalisa@monalisas-MacBook-Pro.local>
2019-04-26 19:37:46 +0530

    initial file added

---
+++ app.js
@@ -0,0 +1,10 @@
+// function sayHello(name){
+//     console.log('Hello ' + name);
+// }
+// sayHello('Mosh');
+
+//  var logger = require('./logger');
+//  logger.log('message');
+
+ var log = require('./logger');
+ log('message');
